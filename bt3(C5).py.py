2d25cb86a973ead83a79ecbba3cf97f662110b11
Khoa học dữ liệu 17A1 <pmhieu.dhkl17a1hn@sv.uneti.edu.vn>
2023-11-09 17:41:19 +0700

    Bài tập chương 5

---
+++ bt3(C5).py.py
@@ -0,0 +1,6 @@
+print("Sữa hộp Vina Milk")
+print("Số lượng: 5")
+so_luong = 5
+print("Đơn giá: 25000")
+don_gia = 25000
+print("Tiền phải trả:"), print(so_luong*don_gia, "vnd")
---
+++ bt4(C5.py.py
@@ -0,0 +1,6 @@
+a = float(input("nhập độ dài cạnh a: "))
+b = float(input("Nhập độ dài cạnh b: "))
+c = float(input("Nhập độ dài cạnh c: "))
+p = (a + b + c) / 2
+S = (p * (p - a) * (p - b) * (p - c))
+print("Diện tích tam giác là:", S)

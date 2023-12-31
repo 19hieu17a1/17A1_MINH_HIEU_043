c445dad381af4037d09a54a43d5905e14370d611
19hieu17a1 <148112099+19hieu17a1@users.noreply.github.com>
2023-12-31 10:07:04 +0700

    Bài tập chương 2

--- Bai2.py
+++ Bai2.py
@@ -5,7 +5,7 @@ try:
             if n == 0:
                 break
             elif n < 0:
-                raise ValueError ("Lỗi số âm!!!!!!")
+                raise ValueError ("Lỗi số âm!!!!!")
             elif not n % 2:
                  raise ValueError ("lỗi nhập chẵn !!!")
             if len(list_n) >= 4 and list_n[-4:] == [n,n,n,n]:
